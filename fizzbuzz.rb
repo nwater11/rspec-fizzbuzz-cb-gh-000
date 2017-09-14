40b5e2bd07cfe4e15202dd9e6d523ec198dcb12f
Jane Smith <5904002440@rumail.ru.ac.th>
2017-09-14 16:02:13 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,7 +3,7 @@ def fizzbuzz(number)
     "Fizz" # Go fizz
   elsif number % 5 == 0
     "Buzz"
-  elsif number % 3 == 0 && number % 5 == 0
+  elsif (number % 3 == 0) && (number % 5 == 0)
     "FizzBuzz"
   else
     "nil"

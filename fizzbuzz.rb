b84be00f47e201568a90e2f5377308b7101fa160
Jane Smith <5904002440@rumail.ru.ac.th>
2017-09-14 16:15:14 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,10 @@
 def fizzbuzz(number)
   if number % 3 == 0 # if the number int is divisible by 3
-    "Fizz" # Go fizz
+    puts "Fizz" # Go fizz
   elsif number % 5 == 0
-    "Buzz"
-  elsif (number % 3 == 0) && (number % 5 == 0)
-    "FizzBuzz"
+    puts "Buzz"
+  elsif number % 3 == 0 && number % 5 == 0
+    puts "FizzBuzz"
   else
     nil
   end

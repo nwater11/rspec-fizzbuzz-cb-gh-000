24912362353a750dafaccf9dff692df6b8e21f0f
Jane Smith <5904002440@rumail.ru.ac.th>
2017-09-14 16:33:52 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,8 +1,6 @@
 def fizzbuzz(number)
   if number % 3 == 0 # if the number int is divisible by 3
      "Fizz" # Go fizz
-  elsif number % 5 == 0
-     "Buzz"
   elsif number % 15 == 0
      "FizzBuzz"
   else
